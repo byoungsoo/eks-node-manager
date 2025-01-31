6b771bac9562fdebe811339e1671a1c9abcd5abc
byoungsoo <skwltg90@naver.com>
2025-01-31 14:10:43 +0900

    Update

--- script/update_pipeline.sh
+++ script/update_pipeline.sh
@@ -1,5 +1,5 @@
-aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/shared-ap2/shared-cfn-cdpl.json --region ap-northeast-2 --profile shared-admin
-aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/shared-ap2/shared-lambda-cdpl.json --region ap-northeast-2 --profile shared-admin
+#aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/shared-ap2/shared-cfn-cdpl.json --region ap-northeast-2 --profile shared-admin
+#aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/shared-ap2/shared-lambda-cdpl.json --region ap-northeast-2 --profile shared-admin
 
 aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ap2/dev-cfn-cdpl.json --region ap-northeast-2 --profile dev-admin
 aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ap2/dev-lambda-cdpl.json --region ap-northeast-2 --profile dev-admin
