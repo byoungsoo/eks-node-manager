fb485293c8ddab202466eed4495e32cb0fbeb374
byoungsoo <skwltg90@naver.com>
2025-02-03 10:49:31 +0900

    Update for us-east-1 region.

--- script/update_pipeline.sh
+++ script/update_pipeline.sh
@@ -4,5 +4,5 @@
 #aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ap2/dev-cfn-cdpl.json --region ap-northeast-2 --profile dev-admin
 #aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ap2/dev-lambda-cdpl.json --region ap-northeast-2 --profile dev-admin
 
-aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ue1/dev-cfn-cdpl.json --region us-east-1 --profile dev-admin
-#aws codepipeline update-pipeline --cli-inpt-json file://../codeseries/cdpl/dev-ue1/dev-lambda-cdpl.json --region us-east-1 --profile dev-admin
+#aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ue1/dev-cfn-cdpl.json --region us-east-1 --profile dev-admin
+aws codepipeline update-pipeline --cli-input-json file://../codeseries/cdpl/dev-ue1/dev-lambda-cdpl.json --region us-east-1 --profile dev-admin
